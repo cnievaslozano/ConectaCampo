68a4e229df5fbfc442c135ac118fdc90e11d8d03
cnievaslozano <cnievaslozano@gmail.com>
2024-07-28 01:13:05 +0200

    Basic User Api

---
+++ springboot/src/main/java/es/conectacampo/springboot/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package es.conectacampo.springboot.repository;
+
+import es.conectacampo.springboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ springboot/src/main/java/es/conectacampo/springboot/service/UserService.java
@@ -0,0 +1,32 @@
+package es.conectacampo.springboot.service;
+
+import es.conectacampo.springboot.model.User;
+import es.conectacampo.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUser(Long id){
+
+        return userRepository.findById(id);
+    }
+
+    public void saveOrUpdate(User user) {
+        userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
