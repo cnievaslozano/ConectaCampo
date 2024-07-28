4356f8933ab0617df8b3c7ce1db6a49a4fd27648
cnievaslozano <cnievaslozano@gmail.com>
2024-07-28 18:57:09 +0200

    add comments

--- springboot/src/main/java/es/conectacampo/springboot/service/UserService.java
+++ springboot/src/main/java/es/conectacampo/springboot/service/UserService.java
@@ -13,19 +13,22 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    // get all users
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
+    // get one user
     public Optional<User> getUser(Long id){
-
         return userRepository.findById(id);
     }
 
+    // save || update user
     public void saveOrUpdate(User user) {
         userRepository.save(user);
     }
 
+    // delete user
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
