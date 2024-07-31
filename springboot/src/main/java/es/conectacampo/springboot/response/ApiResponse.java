9e152738d166d01feaf2d34fa1675c883f7b7f4b
cnievaslozano <cnievaslozano@gmail.com>
2024-07-31 19:43:54 +0200

    Class Api Response

---
+++ springboot/src/main/java/es/conectacampo/springboot/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package es.conectacampo.springboot.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse {
+    private String status;
+    private String message;
+}
