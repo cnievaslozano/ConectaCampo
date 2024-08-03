aec2416ab89d698418160d43dd2a5361fb958d2b
cnievaslozano <cnievaslozano@gmail.com>
2024-08-03 17:21:04 +0200

    ui components

---
+++ react/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout';
+import '../styles/App.css';
+import '../styles/fonts.css';
+
+const Home = () => {
+    return (
+        <Layout>
+            <div className="text-center">
+                <h2 className="text-3xl font-bold mb-4">La mejor plataforma para conectar a los productores agrícolas con los consumidores.</h2>
+            </div>
+        </Layout>
+    )
+}
+
+export default Home;
