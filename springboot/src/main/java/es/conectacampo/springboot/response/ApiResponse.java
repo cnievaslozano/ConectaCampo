f3b28d48b0137c4b723bfb1ada3f5e3dad34bc2d
Daniel Alvarez <danipoal.0@gmail.com>
2024-08-20 18:13:50 +0200

    Change app.properties and constructor apiResponse to work Dani pc

--- springboot/src/main/java/es/conectacampo/springboot/response/ApiResponse.java
+++ springboot/src/main/java/es/conectacampo/springboot/response/ApiResponse.java
@@ -2,12 +2,15 @@ package es.conectacampo.springboot.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class ApiResponse {
     private String status;
     private String message;
+
+    public ApiResponse(String success, String userFollowedSuccessfully) {
+    }
 }
