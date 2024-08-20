75c0b3a24f219d25725e9c0702965d8e19b850ae
cnievaslozano <cnievaslozano@gmail.com>
2024-08-20 19:16:45 +0200

    Dani Login

--- react/src/components/common/Button.tsx
+++ react/src/components/common/Button.tsx
@@ -1,9 +1,20 @@
-const Button = ( {text, className}:any ) => {
-  return (
-    <button className={`${className} bg-[#8AA86E] dark:bg-[#3E5C33] border-[#8AA86E] dark:border-[#3E5C33] border rounded-full inline-flex items-center justify-center font-bold text-center text-base  text-white hover:bg-[#A1C77D] dark:hover:bg-[#4B763E] hover:border-[#A1C77D] dark:hover:border-[#4B763E] disabled:bg-gray-300 disabled:border-gray-300 disabled:text-gray-500 transition-colors duration-300`}>
-      {text} 
-    </button>
-  )
+import React from 'react';
+
+interface ButtonProps {
+  text: string;
+  className: string;
+  type?: "button" | "submit" | "reset"; // El atributo type es opcional y puede ser de tipo "button", "submit" o "reset"
 }
 
-export default Button
+const Button: React.FC<ButtonProps> = ({ text, className, type = "button" }) => {
+  return (
+    <button
+      type={type}
+      className={`${className} bg-[#8AA86E] dark:bg-[#3E5C33] border-[#8AA86E] dark:border-[#3E5C33] border rounded-full inline-flex items-center justify-center font-bold text-center text-base text-white hover:bg-[#A1C77D] dark:hover:bg-[#4B763E] hover:border-[#A1C77D] dark:hover:border-[#4B763E] disabled:bg-gray-300 disabled:border-gray-300 disabled:text-gray-500 transition-colors duration-300`}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
