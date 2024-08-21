2080233f56b015edb6f947c2bc4b5e6c37cd1dcb
cnievaslozano <cnievaslozano@gmail.com>
2024-08-21 19:27:03 +0200

    register styles

--- react/src/pages/Register.tsx
+++ react/src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Button from "@components/common/Button";
 import iconConectaCampo from "@assets/conectaCampo.png";
+import illustration from "@assets/ilustrationRegister.png";
 
 const InputBox = ({ type, placeholder, name }: any) => {
   return (
@@ -9,7 +10,7 @@ const InputBox = ({ type, placeholder, name }: any) => {
         type={type}
         placeholder={placeholder}
         name={name}
-        className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-body-color outline-none focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white"
+        className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:text-white"
       />
     </div>
   );
@@ -17,75 +18,113 @@ const InputBox = ({ type, placeholder, name }: any) => {
 
 const Register = () => {
   return (
-    <section className="bg-lightGreen3 py-20 dark:bg-dark lg:py-[120px]">
-      <div className={`container mx-auto`}>
-        <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4">
-            <div className="relative mx-auto max-w-[525px] overflow-hidden rounded-lg bg-white px-10 py-16 text-center dark:bg-dark-2 sm:px-12 md:px-[60px]">
-              <div className="mb-10 text-center md:mb-16">
-                <Link
-                  to="/"
-                  className="flex justify-center items-center mb-4 lg:mb-0 w-full"
-                >
-                  <img
-                    src={iconConectaCampo}
-                    className="h-20"
-                    alt="ConectaCampo Logo"
-                  />
-                  <div className="flex flex-col justify-center items-center text-3xl font-semibold whitespace-nowrap text-black">
-                    <span className="leading-none mb-0">Conecta</span>
-                    <span className="leading-none mt-0">Campo</span>
-                  </div>
-                </Link>
+    <div className="flex justify-center items-center min-h-screen bg-lightGreen3">
+      <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-5xl">
+        {/* Imagen de la izquierda con altura específica */}
+        <div className="py-10 w-full flex justify-center items-center lg:w-1/2 overflow-hidden">
+          <img
+            src={illustration}
+            alt="Ilustración de Registro"
+            className="transition-transform duration-300 ease-in-out transform hover:scale-105"
+          />
+        </div>
+
+        {/* Contenedor del formulario */}
+        <div className="w-full px-6 py-4 md:px-8 lg:w-1/2">
+          <div className="flex justify-center mx-auto">
+            <img
+              className="lg:h-24 sm:h-8 transition-transform duration-300 ease-in-out transform hover:scale-105"
+              src={iconConectaCampo}
+              alt="ConectaCampo Logo"
+            />
+          </div>
+
+          <p className="mt-3 text-3xl text-center text-gray-600 dark:text-gray-200">
+            ¡Regístrate!
+          </p>
+
+          <form className="mt-4">
+            {/* Campos del formulario */}
+            <div className="flex gap-4">
+              <div className="flex-1">
+                <InputBox type="text" placeholder="Nombre*" name="name" />
               </div>
-
-              <form method="post" encType="multipart/form-data">
-                <p className="text-start">Datos de cuenta: </p>
-                <InputBox type="user" name="username" placeholder="Usuario" />
-                <InputBox
-                  type="password"
-                  name="password"
-                  placeholder="Contraseña"
-                />
-
-                <p className="text-start">Datos Personales: </p>
-                <InputBox type="text" name="name" placeholder="Nombre" />
-                <InputBox type="text" name="surname" placeholder="Apellidos" />
-                <InputBox type="email" name="email" placeholder="Email" />
-                <InputBox
-                  type="number"
-                  name="telephone"
-                  placeholder="Telefono"
-                />
-
-                {/* Imagen de Perfil Opcional */}
-                <div className="mb-4">
-                  <label
-                    className="block text-start mb-2"
-                    htmlFor="profileImage"
-                  >
-                    Imagen de Perfil:
-                  </label>
-                  <input
-                    type="file"
-                    name="profileImage"
-                    id="profileImage"
-                    accept="image/*"
-                    className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-sm text-body-color outline-none focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white"
-                  />
-                </div>
-
-                <div className="mb-10">
-                  <Button text="Registrarse" className="p-3" />
-                </div>
-              </form>
-
-              <div>{/* Aquí puedes agregar otros enlaces o contenido */}</div>
+              <div className="flex-1">
+                <InputBox type="text" placeholder="Apellido*" name="surname" />
+              </div>
             </div>
+
+            <InputBox
+              type="email"
+              placeholder="Correo Electrónico*"
+              name="email"
+            />
+            <InputBox
+              type="text"
+              placeholder="Nombre de Usuario*"
+              name="username"
+            />
+
+            <InputBox
+              type="password"
+              placeholder="Contraseña*"
+              name="password"
+            />
+
+            <div className="flex gap-4">
+              <div className="flex-1">
+                <InputBox type="tel" placeholder="Teléfono" name="telephone" />
+              </div>
+              <div className="flex-1">
+                <InputBox type="text" placeholder="Ciudad*" name="city" />
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="file"
+                name="profileImage"
+                className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-body-color outline-none dark:border-dark-3 dark:text-white"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+                Tipo de Cuenta
+              </label>
+              <select
+                name="accountType"
+                className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:text-white"
+              >
+                <option value="normal">Normal</option>
+                <option value="vendedor">Vendedor</option>
+              </select>
+            </div>
+
+            <div className="mt-6">
+              <Button
+                className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+                text="Registrar"
+                type="submit"
+              />
+            </div>
+          </form>
+
+          <div className="flex items-center justify-between mt-4">
+            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+
+            <Link
+              to="/signIn"
+              className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
+            >
+              ¿Ya tienes cuenta? Inicia sesión
+            </Link>
+
+            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
