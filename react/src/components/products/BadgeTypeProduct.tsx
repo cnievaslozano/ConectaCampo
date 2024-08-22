10090e302e1ca7488ffeaba3da2d107708a49739
Daniel Alvarez <danipoal.0@gmail.com>
2024-08-22 12:38:12 +0200

    BadgeProduct done + fixes in cards mockup

--- react/src/components/products/BadgeTypeProduct.tsx
+++ react/src/components/products/BadgeTypeProduct.tsx
@@ -2,12 +2,48 @@ interface badgeProps{
     type: string,
     className?: string
 }
-const BadgeTypeProduct: React.FC<badgeProps> = (type, className) => {
-
-if (type === "verdura") {
+const BadgeTypeProduct: React.FC<badgeProps> = ({type, className}) => {
+//Tipos : Verduras Frutas Lacteos Granos semillas Aceites Miel
+if (type.toLowerCase() === "verduras") {
     return(
         <span className={`bg-green-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 ${className}`}>
         Verdura
+        </span>
+    )
+} else if (type.toLowerCase() === "frutas") {
+    return(
+        <span className={`bg-yellow-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 ${className}`}>
+        Fruta
+        </span>
+    )
+}else if (type.toLowerCase() === "lacteos") {
+    return(
+        <span className={`bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 ${className}`}>
+        Lacteo
+        </span>
+    )
+}else if (type.toLowerCase() === "granos") {
+    return(
+        <span className={`bg-orange-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 ${className}`}>
+        Grano
+        </span>
+    )
+}else if (type.toLowerCase() === "semillas") {
+    return(
+        <span className={`bg-stone-300 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 ${className}`}>
+        Semilla
+        </span>
+    )
+}else if (type.toLowerCase() === "aceites") {
+    return(
+        <span className={`bg-lime-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 ${className}`}>
+        Aceite
+        </span>
+    )
+}else if (type.toLowerCase() === "miel") {
+    return(
+        <span className={`bg-red-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 ${className}`}>
+        MIel
         </span>
     )
 }
--- react/src/components/products/CardProduct.tsx
+++ react/src/components/products/CardProduct.tsx
@@ -1,12 +1,27 @@
 import Button from "@components/common/Button";
 import { Link } from "react-router-dom";
+import BadgeTypeProduct from "./BadgeTypeProduct";
+
+interface product { //TODO falta implementarlo en el card product. Los fetchs vendran de cada grid en funcion de lo que quieran
+    id: string,
+    name: string,
+    description: string,
+    price: string,
+    quantity:number,
+    userId: string,
+    categoryIds: string,
+    image: string,
+    schedule: string,
+    addres: string,
+}
 
 const CardProduct = (produc:any) => {
   const product = {
     id: 1,
     name: "Tomates camperos",
-    description:"Sandias frescas de El priorat",
-    price: "599",
+    description:"Sandias frescas plantadas al aire libre, sin conservantes ni pesticidas.",
+    price: "3,49",
+    quantity: 3,
     userId: "Juan Gonzalez",
     categoryIds: "Verduras",
     image:
@@ -21,19 +36,17 @@ const CardProduct = (produc:any) => {
       className="h-full min-h-full max-h-full flex-shrink-0 flex flex-col items-center overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105"
     >
       <div className="w-full max-w-2xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img
-            className="p-4 rounded-t-lg"
-            src={product.image}
-            alt={product.name}
-          />
-        </a>
+        <img
+        className="p-4 rounded-t-lg"
+        src={product.image}
+        alt={product.name}
+        />
         <div className="px-4 pb-4">
-          <a href="#">
+          <Link to="/product">
             <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
               {product.name}
             </h5>
-          </a>
+          </Link>
           <div className="flex items-center mt-1 mb-4">
             <div>
               <Link to="/profile">
@@ -44,19 +57,19 @@ const CardProduct = (produc:any) => {
             </div>
 
             <div>
-              <span className="bg-green-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-                {product.categoryIds}
-              </span>
+                <BadgeTypeProduct type={product.categoryIds}/>
             </div>
           </div>
           <div className="text-sm mb-4">
             {product.description}
           </div>
           <div className="flex items-center justify-between">
-            <span className="text-xl font-bold text-gray-900 dark:text-white">
-              {product.price}€
+            <span className="text-base font-bold text-gray-800 dark:text-white">
+                {product.price} €/kg - {product.quantity}kg
+                
+               
             </span>
-            <Button text="Ver mas" className="py-2 px-4" />
+            <Button text="Ver mas" to="/product" className="py-2 px-4" />
           </div>
         </div>
       </div>
