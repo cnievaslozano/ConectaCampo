fe8d3baf73ac1109186a4e1258cfb63e260bf5b4
cnievaslozano <cnievaslozano@gmail.com>
2024-08-24 20:33:07 +0200

    add navigate to 404

--- react/src/components/user/ProfileFollowList.tsx
+++ react/src/components/user/ProfileFollowList.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Layout from '@components/layout/Layout';
-import Container from '@components/common/Container';
-import { Link, useParams } from 'react-router-dom';
-import ProfileCard from '@components/user/ProfileCard';
-import ErrorComp from '@components/layout/Error';
+import React, { useState, useEffect } from "react";
+import Layout from "@components/layout/Layout";
+import Container from "@components/common/Container";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import ProfileCard from "@components/user/ProfileCard";
 
 interface User {
   id: number;
@@ -28,6 +27,7 @@ const ProfileFollowList = ({ type }: { type?: string }) => {
   const [user, setUser] = useState<User | null>(null);
   const [userList, setUserList] = useState<number[]>([]);
   const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const navigate = useNavigate();
 
   const fetchAndFilterUsers = (allUsers: User[]) => {
     const filtered = allUsers.filter((user) => userList.includes(user.id));
@@ -42,25 +42,37 @@ const ProfileFollowList = ({ type }: { type?: string }) => {
     };
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/user/all", requestOptions);
+      const response = await fetch(
+        "http://localhost:8080/api/v1/user/all",
+        requestOptions
+      );
       const result = await response.json();
       console.log("Fetched Users:", result); // Verifica que el fetch trae datos
-      const userFound = result.find((item: { username: string | undefined; }) => item.username === usernameUrl);
+      const userFound = result.find(
+        (item: { username: string | undefined }) =>
+          item.username === usernameUrl
+      );
       if (userFound) {
         setUser(userFound);
 
         let ids: number[] = [];
         if (type === "following") {
-          ids = userFound.following.map((follower: { id: number }) => follower.id);
+          ids = userFound.following.map(
+            (follower: { id: number }) => follower.id
+          );
         } else if (type === "followers") {
-          ids = userFound.followers.map((follower: { id: number }) => follower.id);
+          ids = userFound.followers.map(
+            (follower: { id: number }) => follower.id
+          );
         }
 
         console.log("User List IDs:", ids); // Verifica si los IDs se obtienen correctamente
-        setUserList(ids); 
+        setUserList(ids);
 
         // Filtrar usuarios inmediatamente después de obtener la lista de IDs
         fetchAndFilterUsers(result);
+      } else {
+        navigate("/404");
       }
     } catch (error) {
       console.error("Error fetching profile data:", error);
@@ -80,10 +92,12 @@ const ProfileFollowList = ({ type }: { type?: string }) => {
   if (user) {
     return (
       <Layout>
-        <Container className=''>
+        <Container className="">
           <div>
-            <p>UserList = {userList.join(', ')}</p>
-            <Link to="/profile"><h1 className='text-3xl text-center mb-10'>Rafel Seguidores</h1></Link>
+            <p>UserList = {userList.join(", ")}</p>
+            <Link to="/profile">
+              <h1 className="text-3xl text-center mb-10">Rafel Seguidores</h1>
+            </Link>
             {filteredUsers.length > 0 ? (
               <>
                 {filteredUsers.map((person) => (
@@ -98,7 +112,7 @@ const ProfileFollowList = ({ type }: { type?: string }) => {
       </Layout>
     );
   } else {
-    return <ErrorComp />;
+    return null;
   }
 };
 
--- react/src/pages/Profile.tsx
+++ react/src/pages/Profile.tsx
@@ -4,9 +4,8 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import Container from "@components/common/Container";
 import Button from "@components/common/Button";
 import CardProduct from "@components/products/CardProduct";
-import defaultImage from "@assets/user/defaultUser.webp"
-import '@styles/Profile.css'
-import ErrorComp from "@components/layout/Error";
+import defaultImage from "@assets/user/defaultUser.webp";
+import "@styles/Profile.css";
 
 interface User {
   id: number;
@@ -46,10 +45,12 @@ interface Role {
   name: string;
 }
 
-const Profile = () => { 
+const Profile = () => {
   const [user, setUser] = useState<User | null>(null);
-  const locationUrl = useLocation(); 
+  const [loading, setLoading] = useState(true); // Estado para manejar el spinner de carga
+  const locationUrl = useLocation();
   const userNameUrl = locationUrl.pathname.split("/").filter(Boolean).pop();
+  const navigate = useNavigate();
 
   const handleProfile = async () => {
     const requestOptions = {
@@ -58,40 +59,67 @@ const Profile = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/user/all", requestOptions);
+      const response = await fetch(
+        "http://localhost:8080/api/v1/user/all",
+        requestOptions
+      );
       const result = await response.json();
-      const userFound = result.find((item: { username: string | undefined; }) => item.username === userNameUrl);
-      setUser(userFound); 
+      const userFound = result.find(
+        (item: { username: string | undefined }) =>
+          item.username === userNameUrl
+      );
+
+      if (userFound) {
+        setUser(userFound);
+      } else {
+        navigate("/404");
+      }
     } catch (error) {
       console.error("Error fetching profile data:", error);
+      navigate("/404");
+    } finally {
+      setLoading(false); // Detiene el spinner una vez que la solicitud ha terminado
     }
   };
 
   useEffect(() => {
     handleProfile();
-  }, [userNameUrl]); // Se ejecuta cada vez que cambia el username en la URL
+  }, [userNameUrl]);
 
-  if(user){
+  if (loading) {
+    // Mostrar spinner mientras se carga la data
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+      </div>
+    );
+  }
+
+  if (user) {
     return (
       <Layout>
         <Container className="">
           <div className="profile-header">
-            <img src={user.profileImage || defaultImage} alt="Profile" className="profile-image" title={user.username} />
+            <img
+              src={user.profileImage || defaultImage}
+              alt="Profile"
+              className="profile-image"
+              title={user.username}
+            />
             <div className="profile-info">
               <h1 className="profile-name">{user.name + " " + user.surname}</h1>
               <p className="profile-description">{user.aboutMe}</p>
               <div className="flex justify-between">
-              <p className="profile-location">{user.city}</p>
-              {localStorage.getItem("username") === userNameUrl ? (
-                <Link to="/addProduct">
-                  <Button
-                    text="Añadir Publicación"
-                    className="rounded-md p-1 text-gray-100"
-                  ></Button>
-                </Link>
-              ) : null}
-            </div>
-
+                <p className="profile-location">{user.city}</p>
+                {localStorage.getItem("username") === userNameUrl ? (
+                  <Link to="/addProduct">
+                    <Button
+                      text="Añadir Publicación"
+                      className="rounded-md p-1 text-gray-100"
+                    ></Button>
+                  </Link>
+                ) : null}
+              </div>
             </div>
             <div className="profile-info-stats">
               <div className="stat-container">
@@ -100,13 +128,13 @@ const Profile = () => {
               </div>
               <div className="stat-container">
                 <p className="stat-name">Seguidos</p>
-                <Link to={"/profile/"+ user.username+"/followList"}>
+                <Link to={"/profile/" + user.username + "/followList"}>
                   <p className="stat-indicator">{user.following.length}</p>
                 </Link>
               </div>
               <div className="stat-container">
                 <p className="stat-name">Seguidores</p>
-                <Link to={"/profile/"+ user.username+"/followerList"}>
+                <Link to={"/profile/" + user.username + "/followerList"}>
                   <p className="stat-indicator">{user.followers.length}</p>
                 </Link>
               </div>
@@ -121,16 +149,14 @@ const Profile = () => {
           <h1 className="text-xl mt-10 mb-5">Favoritos guardados</h1>
           <div className="items-grid">
             {user.products.map((item, index) => (
-              <CardProduct key={item.id} prod={item}/>
+              <CardProduct key={item.id} prod={item} />
             ))}
           </div>
         </Container>
       </Layout>
     );
   } else {
-    return(
-      <ErrorComp />
-    );
+    return null; // En caso de que no haya usuario y se haya manejado la redirección, no mostrar nada
   }
 };
 
