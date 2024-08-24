3674b16ac0c6ae95dd634324723a1161149fb989
cnievaslozano <cnievaslozano@gmail.com>
2024-08-24 20:33:41 +0200

    Formater

--- react/src/pages/About.tsx
+++ react/src/pages/About.tsx
@@ -8,173 +8,171 @@ import ValueImage2 from "@assets/about/honest.webp";
 import ValueImage3 from "@assets/about/jus.webp";
 import ValueImage4 from "@assets/about/inn.webp";
 import Layout from "@components/layout/Layout";
+import Container from "@components/common/Container";
 
 const About = () => {
   return (
     <Layout>
-      <div className="bg-lightGreen3 text-black font-sans">
-        <main className="bg-white text-center max-w-5xl mx-auto my-8 py-16 px-4 shadow-lg rounded-lg">
-          <h1 className="text-4xl font-serif font-bold text-2d572c mb-4 text-center text-[#006400]">
-            Sobre Nosotros
-          </h1>
-          <p className="text-black max-w-3xl mx-auto">
-            Somos tres estudiantes apasionados por la informática y
-            comprometidos con el desarrollo sostenible y la justicia económica.
-            Nos hemos enfrentado a la creciente problemática de los productos
-            importados de bajo costo que afectan negativamente a nuestros
-            agricultores locales y a la economía regional.
-            <br />
-            <br />
-            Con la firme convicción de que es posible cambiar esta realidad,
-            hemos decidido poner en marcha una plataforma web innovadora que
-            conecta directamente a los agricultores con los consumidores
-            finales. Eliminamos los intermediarios, promoviendo una economía
-            local más justa y equitativa.
-            <br />
-            <br />
-            Creemos que al facilitar el acceso directo entre productores y
-            consumidores, no solo garantizamos precios justos, sino que también
-            apoyamos y fortalecemos la agricultura local. Nuestra misión es
-            proporcionar una solución tecnológica que beneficie a todos los
-            involucrados en la cadena de suministro alimentaria, fomentando una
-            economía regional más robusta y sostenible.
-          </p>
+      <Container>
+        <h1 className="text-4xl font-serif font-bold text-2d572c mb-4 text-center text-[#006400]">
+          Sobre Nosotros
+        </h1>
+        <p className="text-black max-w-3xl mx-auto">
+          Somos tres estudiantes apasionados por la informática y comprometidos
+          con el desarrollo sostenible y la justicia económica. Nos hemos
+          enfrentado a la creciente problemática de los productos importados de
+          bajo costo que afectan negativamente a nuestros agricultores locales y
+          a la economía regional.
+          <br />
+          <br />
+          Con la firme convicción de que es posible cambiar esta realidad, hemos
+          decidido poner en marcha una plataforma web innovadora que conecta
+          directamente a los agricultores con los consumidores finales.
+          Eliminamos los intermediarios, promoviendo una economía local más
+          justa y equitativa.
+          <br />
+          <br />
+          Creemos que al facilitar el acceso directo entre productores y
+          consumidores, no solo garantizamos precios justos, sino que también
+          apoyamos y fortalecemos la agricultura local. Nuestra misión es
+          proporcionar una solución tecnológica que beneficie a todos los
+          involucrados en la cadena de suministro alimentaria, fomentando una
+          economía regional más robusta y sostenible.
+        </p>
 
-          <img
-            src={GroupImage}
-            alt="Nuestro Equipo"
-            className="w-3/4 h-auto mt-8 mb-12 rounded-lg mx-auto"
-          />
+        <img
+          src={GroupImage}
+          alt="Nuestro Equipo"
+          className="w-3/4 h-auto mt-8 mb-12 rounded-lg mx-auto"
+        />
 
-          <section className="flex flex-wrap justify-around mt-4">
-            <div className="flex-1 max-w-xs mx-2">
-              <h2 className="text-2xl font-serif font-bold text-2d572c text-[#006400]">
-                Nuestra Misión
-              </h2>
-              <p className="text-black mt-2">
-                Nuestra misión es apoyar a los agricultores locales y reducir el
-                desperdicio de alimentos, promoviendo prácticas agrícolas
-                sostenibles. Nos comprometemos a proporcionar una plataforma
-                donde los agricultores puedan vender directamente a los
-                consumidores, asegurando precios justos y reduciendo los costos
-                adicionales impuestos por los intermediarios.
-              </p>
-            </div>
-            <div className="flex-1 max-w-xs mx-2">
-              <h2 className="text-2xl font-serif font-bold text-2d572c text-[#006400]">
-                Nuestra Visión
-              </h2>
-              <p className="text-black mt-2">
-                Nuestra visión es ser el puente entre agricultores y
-                consumidores, facilitando un comercio justo y local. Aspiramos a
-                crear una comunidad conectada donde los consumidores puedan
-                acceder a productos frescos y de alta calidad, mientras apoyan a
-                los agricultores locales y contribuyen a una economía más
-                sostenible.
-              </p>
-            </div>
-          </section>
-
-          <section className="my-8">
-            <h2 className="text-2xl font-serif font-bold text-2d572c mb-4 text-center text-[#006400]">
-              Nuestros Valores
+        <section className="flex flex-wrap justify-around mt-4">
+          <div className="flex-1 max-w-xs mx-2">
+            <h2 className="text-2xl font-serif font-bold text-2d572c text-[#006400]">
+              Nuestra Misión
             </h2>
-            <div className="flex justify-between mx-8">
-              <div className="text-center flex-1 mx-4">
-                <img
-                  src={ValueImage1}
-                  alt="Sostenibilidad"
-                  className="w-32 h-auto mb-2 mx-auto"
-                />
-                <p className="text-black font-semibold">Sostenibilidad</p>
-                <p className="text-black mt-2">
-                  Promovemos prácticas que respeten el medio ambiente.
-                </p>
-              </div>
-              <div className="text-center flex-1 mx-4">
-                <img
-                  src={ValueImage2}
-                  alt="Transparencia"
-                  className="w-32 h-auto mb-2 mx-auto"
-                />
-                <p className="text-black font-semibold">Transparencia</p>
-                <p className="text-black mt-2">
-                  Creemos en una comunicación abierta y honesta.
-                </p>
-              </div>
-              <div className="text-center flex-1 mx-4">
-                <img
-                  src={ValueImage3}
-                  alt="Justicia Económica"
-                  className="w-32 h-auto mb-2 mx-auto"
-                />
-                <p className="text-black font-semibold">Justicia Económica</p>
-                <p className="text-black mt-2">
-                  Nos esforzamos por garantizar un comercio justo para todos.
-                </p>
-              </div>
-              <div className="text-center flex-1 mx-4">
-                <img
-                  src={ValueImage4}
-                  alt="Innovación"
-                  className="w-32 h-auto mb-2 mx-auto"
-                />
-                <p className="text-black font-semibold">Innovación</p>
-                <p className="text-black mt-2">
-                  Buscamos constantemente mejorar y adaptarnos a nuevas
-                  tecnologías.
-                </p>
-              </div>
-            </div>
-          </section>
-
-          <section className="my-4">
-            <h2 className="text-2xl font-serif font-bold text-2d572c mb-4 text-center text-[#006400]">
-              Nuestro Equipo
-            </h2>
-            <div className="flex justify-around flex-wrap">
-              <div className="text-center mx-4">
-                <img
-                  src={CristianImage}
-                  alt="Cristian"
-                  className="w-20 h-20 rounded-full mx-auto"
-                />
-                <p className="text-black mt-2">Cristian</p>
-              </div>
-              <div className="text-center mx-4">
-                <img
-                  src={DaniImage}
-                  alt="Dani"
-                  className="w-20 h-20 rounded-full mx-auto"
-                />
-                <p className="text-black mt-2">Dani</p>
-              </div>
-              <div className="text-center mx-4">
-                <img
-                  src={MiryamImage}
-                  alt="Miryam"
-                  className="w-20 h-20 rounded-full mx-auto"
-                />
-                <p className="text-black mt-2">Miryam</p>
-              </div>
-            </div>
-          </section>
-
-          <section className="my-8 text-center">
-            <h2 className="text-2xl font-serif font-bold text-2d572c mb-4 text-[#006400]">
-              Únete a Nosotros
-            </h2>
-            <p className="text-black max-w-3xl mx-auto mb-4">
-              ¿Te interesa lo que hacemos? ¡Únete a nuestra comunidad!
-              Suscríbete a nuestro boletín para recibir las últimas noticias,
-              ofertas exclusivas, y mucho más.
+            <p className="text-black mt-2">
+              Nuestra misión es apoyar a los agricultores locales y reducir el
+              desperdicio de alimentos, promoviendo prácticas agrícolas
+              sostenibles. Nos comprometemos a proporcionar una plataforma donde
+              los agricultores puedan vender directamente a los consumidores,
+              asegurando precios justos y reduciendo los costos adicionales
+              impuestos por los intermediarios.
             </p>
-            <button className="bg-[#006400] text-white font-bold py-2 px-6 rounded hover:bg-green-700">
-              Suscribirse
-            </button>
-          </section>
-        </main>
-      </div>
+          </div>
+          <div className="flex-1 max-w-xs mx-2">
+            <h2 className="text-2xl font-serif font-bold text-2d572c text-[#006400]">
+              Nuestra Visión
+            </h2>
+            <p className="text-black mt-2">
+              Nuestra visión es ser el puente entre agricultores y consumidores,
+              facilitando un comercio justo y local. Aspiramos a crear una
+              comunidad conectada donde los consumidores puedan acceder a
+              productos frescos y de alta calidad, mientras apoyan a los
+              agricultores locales y contribuyen a una economía más sostenible.
+            </p>
+          </div>
+        </section>
+
+        <section className="my-8">
+          <h2 className="text-2xl font-serif font-bold text-2d572c mb-4 text-center text-[#006400]">
+            Nuestros Valores
+          </h2>
+          <div className="flex justify-between mx-8">
+            <div className="text-center flex-1 mx-4">
+              <img
+                src={ValueImage1}
+                alt="Sostenibilidad"
+                className="w-32 h-auto mb-2 mx-auto"
+              />
+              <p className="text-black font-semibold">Sostenibilidad</p>
+              <p className="text-black mt-2">
+                Promovemos prácticas que respeten el medio ambiente.
+              </p>
+            </div>
+            <div className="text-center flex-1 mx-4">
+              <img
+                src={ValueImage2}
+                alt="Transparencia"
+                className="w-32 h-auto mb-2 mx-auto"
+              />
+              <p className="text-black font-semibold">Transparencia</p>
+              <p className="text-black mt-2">
+                Creemos en una comunicación abierta y honesta.
+              </p>
+            </div>
+            <div className="text-center flex-1 mx-4">
+              <img
+                src={ValueImage3}
+                alt="Justicia Económica"
+                className="w-32 h-auto mb-2 mx-auto"
+              />
+              <p className="text-black font-semibold">Justicia Económica</p>
+              <p className="text-black mt-2">
+                Nos esforzamos por garantizar un comercio justo para todos.
+              </p>
+            </div>
+            <div className="text-center flex-1 mx-4">
+              <img
+                src={ValueImage4}
+                alt="Innovación"
+                className="w-32 h-auto mb-2 mx-auto"
+              />
+              <p className="text-black font-semibold">Innovación</p>
+              <p className="text-black mt-2">
+                Buscamos constantemente mejorar y adaptarnos a nuevas
+                tecnologías.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className="my-4">
+          <h2 className="text-2xl font-serif font-bold text-2d572c mb-4 text-center text-[#006400]">
+            Nuestro Equipo
+          </h2>
+          <div className="flex justify-around flex-wrap">
+            <div className="text-center mx-4">
+              <img
+                src={CristianImage}
+                alt="Cristian"
+                className="w-20 h-20 rounded-full mx-auto"
+              />
+              <p className="text-black mt-2">Cristian</p>
+            </div>
+            <div className="text-center mx-4">
+              <img
+                src={DaniImage}
+                alt="Dani"
+                className="w-20 h-20 rounded-full mx-auto"
+              />
+              <p className="text-black mt-2">Dani</p>
+            </div>
+            <div className="text-center mx-4">
+              <img
+                src={MiryamImage}
+                alt="Miryam"
+                className="w-20 h-20 rounded-full mx-auto"
+              />
+              <p className="text-black mt-2">Miryam</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="my-8 text-center">
+          <h2 className="text-2xl font-serif font-bold text-2d572c mb-4 text-[#006400]">
+            Únete a Nosotros
+          </h2>
+          <p className="text-black max-w-3xl mx-auto mb-4">
+            ¿Te interesa lo que hacemos? ¡Únete a nuestra comunidad! Suscríbete
+            a nuestro boletín para recibir las últimas noticias, ofertas
+            exclusivas, y mucho más.
+          </p>
+          <button className="bg-[#006400] text-white font-bold py-2 px-6 rounded hover:bg-green-700">
+            Suscribirse
+          </button>
+        </section>
+      </Container>
     </Layout>
   );
 };
--- react/src/pages/Search.tsx
+++ react/src/pages/Search.tsx
@@ -1,123 +1,147 @@
-import React, { useEffect, useState } from 'react';
-import Layout from '@components/layout/Layout';
-import Container from '@components/common/Container';
-import { Link, useNavigate, useSearchParams } from 'react-router-dom';
-import '@styles/search.css';
-import SearchProductGrid from '@components/products/SearchProductGrid';
+import React, { useEffect, useState } from "react";
+import Layout from "@components/layout/Layout";
+import Container from "@components/common/Container";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import "@styles/search.css";
+import SearchProductGrid from "@components/products/SearchProductGrid";
 
 const Search = () => {
+  //Encuentra el primer input y luego va actualizando la busqueda
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const initialKey = searchParams.get("name") ?? "";
+  const [key, setKey] = useState(initialKey); // Estado para manejar el valor del input
+  const [resultOnEnter, setResultOnEnter] = useState(initialKey);
+  useEffect(() => {
+    setKey(initialKey); // Actualizar el estado si cambia el parámetro de consulta
+  }, [initialKey]);
 
-    //Encuentra el primer input y luego va actualizando la busqueda
-    const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const initialKey = searchParams.get('name') ?? ''  
-    const [key, setKey] = useState(initialKey);  // Estado para manejar el valor del input
-    const [resultOnEnter, setResultOnEnter] = useState(initialKey);
-    useEffect(() => {
-      setKey(initialKey);  // Actualizar el estado si cambia el parámetro de consulta
-    }, [initialKey]);
-  
-    const handleKeyDown = (e : any) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            // Redirigir a la ruta deseada
-            navigate(`/search?name=${key}`);
-            setResultOnEnter(e.target.value)
-          }
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      // Redirigir a la ruta deseada
+      navigate(`/search?name=${key}`);
+      setResultOnEnter(e.target.value);
     }
-    const handleInputChange = (e: any) => {
-        setKey(e.target.value);  // Actualiza el estado conforme el usuario escribe
-    };
-    //TODO Cuando se busca, o se implementan los filtros o el buscar x el nombre ya que no estan en el mismo form.
-    
+  };
+  const handleInputChange = (e: any) => {
+    setKey(e.target.value); // Actualiza el estado conforme el usuario escribe
+  };
+  //TODO Cuando se busca, o se implementan los filtros o el buscar x el nombre ya que no estan en el mismo form.
 
   return (
     <Layout>
-        <Container className=''>
-            <section className='search-main-container'>
+      <Container>
+        <section className="search-main-container">
+          <aside className="search-filters">
+            <form action="/search" method="GET">
+              <div className="distance-filter-div">
+                <label htmlFor="distance">Distancia (km):</label>
+                <input
+                  type="number"
+                  id="distance"
+                  name="distance"
+                  min="0"
+                  max="1000"
+                  placeholder="Hasta cuántos km"
+                />
+              </div>
 
-                <aside className='search-filters'>
-                    <form action="/search" method="GET">
-        
-                        <div className='distance-filter-div'>
-                            <label htmlFor="distance">Distancia (km):</label>
-                            <input type="number" id="distance" name="distance" min="0" max="1000" placeholder="Hasta cuántos km" />
-                        </div>
+              {/*<!-- Filtro de Tipo de Producto -->*/}
+              <div className="product-type-div">
+                <label htmlFor="product-type">Tipo de Producto:</label>
+                <select id="product-type" name="product-type">
+                  <option value="">Seleccionar tipo</option>
+                  <option value="fruits">Frutas</option>
+                  <option value="vegetables">Vegetales</option>
+                  <option value="herbs">Hierbas</option>
+                  <option value="grains">Granos</option>
+                </select>
+              </div>
 
-                        {/*<!-- Filtro de Tipo de Producto -->*/}
-                        <div className='product-type-div'>
-                            <label htmlFor="product-type">Tipo de Producto:</label>
-                            <select id="product-type" name="product-type">
-                            <option value="">Seleccionar tipo</option>
-                            <option value="fruits">Frutas</option>
-                            <option value="vegetables">Vegetales</option>
-                            <option value="herbs">Hierbas</option>
-                            <option value="grains">Granos</option>
-                            </select>
-                        </div>
+              {/*<!-- Filtro de Precio -->*/}
+              <div className="price-filter-div">
+                <label htmlFor="price-range">Rango de Precio:</label>
+                <input
+                  type="number"
+                  id="price-min"
+                  name="price-min"
+                  min="0"
+                  placeholder="Mínimo"
+                />
+                <input
+                  type="number"
+                  id="price-max"
+                  name="price-max"
+                  min="0"
+                  placeholder="Máximo"
+                />
+              </div>
 
-                        {/*<!-- Filtro de Precio -->*/}
-                        <div className='price-filter-div'>
-                            <label htmlFor="price-range">Rango de Precio:</label>
-                            <input type="number" id="price-min" name="price-min" min="0" placeholder="Mínimo" />
-                            <input type="number" id="price-max" name="price-max" min="0" placeholder="Máximo" />
-                        </div>
+              {/*<!-- Filtro de Categoría (Orgánico, Convencional) -->*/}
+              <div className="categoy-filter-div">
+                <label>Categoría:</label>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="organic"
+                    name="category"
+                    value="organic"
+                  />
+                  <label htmlFor="organic">Orgánico</label>
+                </div>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="conventional"
+                    name="category"
+                    value="conventional"
+                  />
+                  <label htmlFor="conventional">Convencional</label>
+                </div>
+              </div>
+              {/*jdjjjdjd*/}
 
-                        {/*<!-- Filtro de Categoría (Orgánico, Convencional) -->*/}
-                        <div className='categoy-filter-div'>
-                            <label>Categoría:</label>
-                            <div>
-                            <input type="checkbox" id="organic" name="category" value="organic" />
-                            <label htmlFor="organic">Orgánico</label>
-                            </div>
-                            <div>
-                            <input type="checkbox" id="conventional" name="category" value="conventional" />
-                            <label htmlFor="conventional">Convencional</label>
-                            </div>
-                        </div>
-                        {/*jdjjjdjd*/} 
-                        
-                        <div className='availability-filter-div'>
-                            <label htmlFor="availability">Disponibilidad:</label>
-                            <select id="availability" name="availability">
-                            <option value="">Seleccionar</option>
-                            <option value="in-stock">En stock</option>
-                            <option value="pre-order">Preordenar</option>
-                            </select>
-                        </div>
+              <div className="availability-filter-div">
+                <label htmlFor="availability">Disponibilidad:</label>
+                <select id="availability" name="availability">
+                  <option value="">Seleccionar</option>
+                  <option value="in-stock">En stock</option>
+                  <option value="pre-order">Preordenar</option>
+                </select>
+              </div>
 
-                        {/*<!-- Filtro de Origen -->*/}
-                        <div className='origin-filter-div'>
-                            <label htmlFor="origin">Origen del Producto:</label>
-                            <select id="origin" name="origin">
-                            <option value="">Seleccionar origen</option>
-                            <option value="local">Local</option>
-                            <option value="imported">Importado</option>
-                            </select>
-                        </div>
+              {/*<!-- Filtro de Origen -->*/}
+              <div className="origin-filter-div">
+                <label htmlFor="origin">Origen del Producto:</label>
+                <select id="origin" name="origin">
+                  <option value="">Seleccionar origen</option>
+                  <option value="local">Local</option>
+                  <option value="imported">Importado</option>
+                </select>
+              </div>
 
-                    
-                        <div className='submit-div'>
-                            <button type="submit">Buscar</button>
-                        </div>
-                    </form>
-
-                </aside>
-                <section className='search-container'>
-                    <input type="text" value={key} onChange={handleInputChange} onKeyDown={handleKeyDown} className='search-name-filter' placeholder='Introduce el nombre del producto'/>
-                    <h3 className='my-4 text-xl'>Resultados de "{resultOnEnter}"</h3>
-                    <SearchProductGrid>
-                        
-                    </SearchProductGrid>
-                </section>
-
-            </section>
-
-        </Container>
+              <div className="submit-div">
+                <button type="submit">Buscar</button>
+              </div>
+            </form>
+          </aside>
+          <section className="search-container">
+            <input
+              type="text"
+              value={key}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+              className="search-name-filter"
+              placeholder="Introduce el nombre del producto"
+            />
+            <h3 className="my-4 text-xl">Resultados de "{resultOnEnter}"</h3>
+            <SearchProductGrid></SearchProductGrid>
+          </section>
+        </section>
+      </Container>
     </Layout>
-
-  )
-
+  );
 };
 
 export default Search;
