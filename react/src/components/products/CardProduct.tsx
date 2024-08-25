b22abb08ede91a88422a7e3cdf522a5beae303cd
cnievaslozano <cnievaslozano@gmail.com>
2024-08-25 02:43:56 +0200

    rework Cris

--- react/src/components/products/CardProduct.tsx
+++ react/src/components/products/CardProduct.tsx
@@ -1,76 +1,69 @@
-import Button from "@components/common/Button";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import BadgeTypeProduct from "./BadgeTypeProduct";
-import { Publication, Product } from "../../types/models";
 import { CardProductProps } from "../../types/props";
 
-//TODO hay que llamar al usuario, alli se encuentran los products, ver el id del producto y luego buscar ese producto en publications para sacar horario etc
+const CardProduct = ({ product }: CardProductProps) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-const CardProduct = ({ prod }: any) => {
-  const product = {
-    id: 1,
-    name: "Tomates camperos",
-    description:
-      "Sandias frescas plantadas al aire libre, sin conservantes ni pesticidas.",
-    price: "3,49",
-    quantity: 3,
-    userId: "pepelozano",
-    categoryIds: "Verduras",
-    image:
-      "https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2022/06/06/16545437734127.jpg",
-    schedule: "3 de Mayo, 15:00 - 17:00",
-    addres: "Priorat, Cataluña",
+  const handleImageLoad = () => {
+    setImageLoaded(true);
   };
 
-  //console.log(prod); De momento funciona
-  if (prod) {
-    return (
-      <div
-        key={prod.id}
-        className="h-full min-h-full max-h-full flex-shrink-0 flex flex-col items-center overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105"
-      >
-        <div className="w-full max-w-2xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  const truncateDescription = (description: string, maxLength: number) => {
+    if (description.length > maxLength) {
+      return description.substring(0, maxLength) + "...";
+    }
+    return description;
+  };
+
+  return (
+    <Link
+      to={`/product/${product.id}`}
+      key={product.id}
+      className="flex-shrink-0 flex flex-col items-center transition-transform duration-300 ease-in-out transform hover:scale-105"
+    >
+      <div className="w-64 min-h-fit bg-white border border-gray-200 rounded-lg shadow">
+        {" "}
+        {/* Ancho fijo */}
+        <div className="relative w-full">
+          {!imageLoaded && (
+            <div className="h-48 absolute inset-0 bg-gray-300 animate-pulse rounded-t-lg"></div>
+          )}
           <img
-            className="p-4 rounded-t-lg"
+            className={`h-48 w-full rounded-t-lg transition-opacity duration-300 ${
+              imageLoaded ? "opacity-100" : "opacity-0"
+            }`}
             src={product.image}
             alt={product.name}
+            onLoad={handleImageLoad}
           />
-          <div className="px-4 pb-4">
-            <Link to="/product">
-              <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
-                {prod.name}
-              </h5>
-            </Link>
-            <div className="flex items-center mt-1 mb-4">
-              <div>
-                <Link to={"/profile/" + product.userId}>
-                  <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
-                    {product.userId}
-                  </span>
-                </Link>
-              </div>
-
-              <div>
-                <BadgeTypeProduct type={product.categoryIds} />
-              </div>
-            </div>
-            <div className="text-sm mb-4">{prod.description}</div>
-            <div className="flex items-center justify-between">
-              <span className="text-base font-bold text-gray-800 dark:text-white">
-                <span className="">{prod.price} €/kg</span> - {prod.quantity}kg
-              </span>
-              <Button text="Ver mas" to="/product" className="py-2 px-4" />
-            </div>
+          {/* Imagen de usuario dentro de la card en la esquina inferior derecha */}
+          <div className="absolute bottom-2 right-2">
+            <img
+              className="w-10 h-10 rounded-full border-2 shadow-xl"
+              src={product.imageUser}
+              alt="User Avatar"
+            />
           </div>
         </div>
+        <div className="px-4 py-4">
+          <div className="flex items-center justify-between">
+            <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
+              {product.name}
+            </h5>
+            <BadgeTypeProduct type={product.categories} />
+          </div>
+          <div className="text-sm mb-4">
+            {truncateDescription(product.description, 50)}
+          </div>
+          <span className="text-base font-bold text-gray-800 dark:text-white">
+            {product.price} €/kg
+          </span>
+        </div>
       </div>
-    );
-  } else {
-    return (
-      <div className="h-full min-h-full max-h-full flex-shrink-0 flex flex-col items-center overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
-        Loading...
-      </div>
-    );
-  }
+    </Link>
+  );
 };
+
 export default CardProduct;
