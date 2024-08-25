a3ade2861855453564e69127ee38c9a43f0b03b2
cnievaslozano <cnievaslozano@gmail.com>
2024-08-25 02:43:20 +0200

    div useless

--- react/src/components/layout/HomeLayout.tsx
+++ react/src/components/layout/HomeLayout.tsx
@@ -2,11 +2,11 @@ import Header from "@components/layout/Header";
 import Footer from "@components/layout/Footer";
 const HomeLayout = ({ children }: any) => {
   return (
-    <div className="">
+    <>
       <Header />
       <main className="mx-auto py-0 px-0 flex-grow bg-white">{children}</main>
       <Footer />
-    </div>
+    </>
   );
 };
 
