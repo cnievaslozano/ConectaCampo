cea4461bf8f2570175095ec967e28080dbbe7c38
cnievaslozano <cnievaslozano@gmail.com>
2024-08-25 02:43:41 +0200

    fix categories & types

--- react/src/components/products/BadgeTypeProduct.tsx
+++ react/src/components/products/BadgeTypeProduct.tsx
@@ -1,23 +1,26 @@
 import { BadgeProps } from "../../types/props";
 
 const badgeStyles: { [key: string]: { bgColor: string; label: string } } = {
-  verduras: { bgColor: "bg-green-100", label: "Verdura" },
-  frutas: { bgColor: "bg-yellow-100", label: "Fruta" },
-  lacteos: { bgColor: "bg-blue-100", label: "Lacteo" },
-  granos: { bgColor: "bg-orange-100", label: "Grano" },
-  semillas: { bgColor: "bg-stone-300", label: "Semilla" },
-  aceites: { bgColor: "bg-lime-100", label: "Aceite" },
-  miel: { bgColor: "bg-red-100", label: "Miel" },
+  Verduras: { bgColor: "bg-green-100", label: "Verdura" },
+  Frutas: { bgColor: "bg-yellow-100", label: "Fruta" },
+  Lácteos: { bgColor: "bg-blue-100", label: "Lácteo" },
+  Granos: { bgColor: "bg-orange-100", label: "Grano" },
+  Hierbas: { bgColor: "bg-green-100", label: "Hierbas" },
+  Semillas: { bgColor: "bg-stone-300", label: "Semillas" },
+  Aceites: { bgColor: "bg-lime-100", label: "Aceite" },
+  Miel: { bgColor: "bg-red-100", label: "Miel" },
+  defaultcategory: { bgColor: "bg-gray-100", label: "otros" },
 };
 
 const BadgeTypeProduct: React.FC<BadgeProps> = ({ type, className }) => {
-  const badgeType = badgeStyles[type.toLowerCase()];
+  const keyType = type.toString();
+  const badgeType = badgeStyles[keyType];
 
-  if (!badgeType) return null; // Retorna null si el tipo no coincide
+  if (!badgeType) return null;
 
   return (
     <span
-      className={`${badgeType.bgColor} text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 ${className}`}
+      className={`${badgeType.bgColor} text-black text-xs font-semibold px-2.5 py-0.5 rounded ms-3 ${className}`}
     >
       {badgeType.label}
     </span>
