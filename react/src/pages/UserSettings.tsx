8ade7f2292ed64dbee71bdf9534dcac3369449c8
Daniel Alvarez <danipoal.0@gmail.com>
2024-08-28 19:51:27 +0200

    POST Congfiguration WORKING

--- react/src/pages/UserSettings.tsx
+++ react/src/pages/UserSettings.tsx
@@ -1,46 +1,94 @@
 import React, { useState } from "react";
 import "@styles/userSettings.css";
 import Layout from "@components/layout/Layout";
+import { User } from "@types/models";
+import { toast, ToastContainer } from "react-toastify";
+
 
 const UserSettings: React.FC = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [role, setRole] = useState("buyer");
-  const [image, setImage] = useState("");
+  let userString = localStorage.getItem("userData");
+  const ownUser: User = JSON.parse(userString ? userString : "");
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const [telephone, setTelephone] = useState(ownUser.telephone);
+  const [city, setCity] = useState(ownUser.city);
+  const [password, setPassword] = useState(ownUser.password);
+  const [aboutMe, setAboutMe] = useState(ownUser.aboutMe);
+  const [pathProfileImage, setPathProfileImage] = useState(ownUser.pathProfileImage);
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Aquí puedes manejar el envío del formulario, por ejemplo, haciendo una llamada a una API.
-    console.log({ name, email, password, role });
+
+    const userData = {
+      telephone,
+      city,
+      password,
+      aboutMe,
+      pathProfileImage,
+    };
+
+    try {
+      //Sacamos la info de el user, y la info de el token
+        const token = localStorage.getItem("token");
+        const ownUserId = ownUser.id;
+        console.log("http://localhost:8080/api/v1/user/" + ownUserId);
+
+        const raw = JSON.stringify({ //Json a pasar en caso de que no exista la info de el ownUser
+          "telephone": "123456789",
+          "city": "Benidorm",
+          "password": "admin",
+          "aboutMe": "Me gusta la playa y los animales",
+          "pathProfileImage": "https://firebasestorage.googleapis.com/v0/b/conectacampo-9691f.appspot.com/o/images%2F1724550483002_3.jpg?alt=media&token=007759ab-cb27-4680-b6fc-197881e0bf47"
+        });
+
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", "Bearer " + token);
+
+      const response = await fetch("http://localhost:8080/api/v1/user/" + ownUserId, {
+        method: "PUT",
+        headers: myHeaders,
+        body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+        toast.error("Error en la peticion")
+        throw new Error("Error en la petición");
+      }
+      toast.success("Datos enviados al post Con exito")
+      console.log("Datos enviados con éxito:", userData);
+    } catch (error) {
+      toast.error("Error envio de datos")
+      console.error("Error al enviar los datos:", error);
+    }
   };
 
   return (
     <Layout>
+      <ToastContainer />
       <div className="settings-container">
         <h2 className="text-xl">Configuración de cuenta</h2>
         <form className="settings-form" onSubmit={handleSubmit}>
           <div className="form-group">
-            <label htmlFor="name">Nombre:</label>
+            <label htmlFor="telephone">Teléfono:</label>
             <input
               type="text"
-              id="name"
-              name="name"
-              placeholder="Introduce tu nuevo nombre"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              id="telephone"
+              name="telephone"
+              placeholder="Introduce tu nuevo teléfono"
+              value={telephone}
+              onChange={(e) => setTelephone(e.target.value)}
               required
             />
           </div>
           <div className="form-group">
-            <label htmlFor="email">Email:</label>
+            <label htmlFor="city">Ciudad:</label>
             <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Introduce tu nuevo email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              id="city"
+              name="city"
+              placeholder="Introduce tu ciudad"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
               required
             />
           </div>
@@ -51,40 +99,33 @@ const UserSettings: React.FC = () => {
               id="password"
               name="password"
               placeholder="Introduce tu nueva contraseña"
-              value={password}
+              value={"admin"}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
           <div className="form-group">
-            <label htmlFor="role">Rol:</label>
-            <select
-              id="role"
-              name="role"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
+            <label htmlFor="aboutMe">Sobre Mí:</label>
+            <textarea
+            className="w-full"
+              id="aboutMe"
+              name="aboutMe"
+              placeholder="Cuéntanos sobre ti"
+              value={aboutMe}
+              onChange={(e) => setAboutMe(e.target.value)}
               required
-            >
-              <option value="buyer">Comprador</option>
-              <option value="seller">Vendedor</option>
-            </select>
+            />
           </div>
-          {/* Imagen de Perfil Opcional */}
-          <div className="mb-4">
-            <label
-              className="block text-start text-body-color mb-2"
-              htmlFor="profileImage"
-            >
-              Imagen de Perfil:
-            </label>
+          <div className="form-group">
+            <label htmlFor="pathProfileImage">Imagen de Perfil (URL):</label>
             <input
-              type="file"
-              name="profileImage"
-              id="profileImage"
-              accept="image/*"
-              className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-sm outline-none focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white"
-              value={image}
-              onChange={(e) => setImage(e.target.value)}
+              type="text"
+              id="pathProfileImage"
+              name="pathProfileImage"
+              placeholder="Introduce la URL de tu imagen de perfil"
+              value={pathProfileImage} //TODO Hacer que cuando se suba la imagen en Firebase, se suba y devuelva el url
+              onChange={(e) => setPathProfileImage(e.target.value)}
+              required
             />
           </div>
           <button type="submit" className="submit-btn">
