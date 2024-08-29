a0548071f2120f27ca27e5b79cf16639310a1e91
Daniel Alvarez <danipoal.0@gmail.com>
2024-08-29 22:35:54 +0200

    Fix nginx + fetching CORS errors

--- springboot/src/main/java/es/conectacampo/springboot/security/WebConfig.java
+++ springboot/src/main/java/es/conectacampo/springboot/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
