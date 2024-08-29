85a51a04da8c9d1e063dac20f9b4a0cd7943d337
Daniel Alvarez <danipoal.0@gmail.com>
2024-08-30 00:26:35 +0200

    fix to try VPS

--- springboot/src/main/java/es/conectacampo/springboot/security/WebConfig.java
+++ springboot/src/main/java/es/conectacampo/springboot/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost", "http://localhost:5173")
+                .allowedOrigins("http://localhost","http://localhost:80", "http://localhost:5173, http://94.143.138.55/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
